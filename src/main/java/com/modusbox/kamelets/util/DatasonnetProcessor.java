7240dd05b8e01dabb80c3e2376d90c44fbbff869
David Borja <>
2022-04-07 18:47:47 +0200

    setting result body message as LinkedHashMap

--- src/main/java/com/modusbox/kamelets/util/DatasonnetProcessor.java
+++ src/main/java/com/modusbox/kamelets/util/DatasonnetProcessor.java
@@ -9,6 +9,8 @@ import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 public class DatasonnetProcessor {
     private static final Logger log = LoggerFactory.getLogger(DatasonnetProcessor.class);
     private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -18,12 +20,15 @@ public class DatasonnetProcessor {
         this.template = template;
     }
 
-    public void process(final Exchange ex) {
+    public void process(final Exchange ex) throws Exception {
         var script = new String(Base64.decodeBase64(template));
         var jsonNodeBody = MAPPER.valueToTree(ex.getMessage().getBody());
         var mapper = new Mapper(script);
         var transformed = mapper
                 .transform(new DefaultDocument<>(jsonNodeBody.toString(), MediaTypes.APPLICATION_JSON));
-        ex.getMessage().setBody(transformed);
+        ex.setProperty(Exchange.CONTENT_TYPE, transformed.getMediaType());
+        var content = transformed.getContent();
+        var contentMap = MAPPER.readValue(content, Map.class);
+        ex.getMessage().setBody(contentMap);
     }
 }
