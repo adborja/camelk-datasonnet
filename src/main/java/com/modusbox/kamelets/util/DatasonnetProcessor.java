e4c1a4641893b4f1bface4795aa44b2fdd2a1f50
David Borja <>
2022-04-07 17:22:41 +0200

    Adding some logs

--- src/main/java/com/modusbox/kamelets/util/DatasonnetProcessor.java
+++ src/main/java/com/modusbox/kamelets/util/DatasonnetProcessor.java
@@ -7,8 +7,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
 import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatasonnetProcessor {
+    private static final Logger log = LoggerFactory.getLogger(DatasonnetProcessor.class);
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private String template;
 
@@ -17,6 +20,9 @@ public class DatasonnetProcessor {
     }
 
     public void process(final Exchange ex) {
+        log.info("Received exchange message: {}", ex.getMessage());
+        log.info("Received exchange message body: {}", ex.getMessage().getBody());
+        log.info("Received exchange message body class: {}", ex.getMessage().getBody().getClass());
         var jsonNodeBody = ex.getMessage().getBody(JsonNode.class);
         var script = new String(Base64.decodeBase64(template));
         var mapper = new Mapper(script);
