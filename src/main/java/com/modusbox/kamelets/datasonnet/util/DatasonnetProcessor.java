100a3522550d6f73d26a650284d7ab2c23d3d4ce
David Borja <>
2022-04-14 01:35:11 +0200

    Adding logs

--- src/main/java/com/modusbox/kamelets/datasonnet/util/DatasonnetProcessor.java
+++ src/main/java/com/modusbox/kamelets/datasonnet/util/DatasonnetProcessor.java
@@ -25,7 +25,7 @@ public class DatasonnetProcessor {
     }
 
     public void process(final Exchange ex) throws Exception {
-        log.info("context props: {}" , ex.getContext().getPropertiesComponent().getLocalProperties());
+        log.info("locations props: {}" , ex.getContext().getPropertiesComponent().getLocations());
         log.info("Exchange Properties: {}", ex.getAllProperties());
         var script = new String(Base64.decodeBase64(template));
         var exchangeWrapper = ExchangeWrapper.fromExchange(ex);
