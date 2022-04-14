f1536e79f9b33ab4a78e4efae8f2512424a1b159
David Borja <>
2022-04-14 19:16:42 +0200

    Adding ExtractPropertiesProcessor

--- src/main/java/com/modusbox/kamelets/ExtractPropertiesProcessor.java
+++ src/main/java/com/modusbox/kamelets/ExtractPropertiesProcessor.java
@@ -1,5 +1,6 @@
 package com.modusbox.kamelets;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
@@ -9,6 +10,9 @@ import org.slf4j.LoggerFactory;
 public class ExtractPropertiesProcessor {
     private static final Logger log = LoggerFactory.getLogger(ExtractPropertiesProcessor.class);
     private static final ObjectMapper MAPPER = new ObjectMapper();
+    static {
+        MAPPER.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+    }
 
     private String property;
 
@@ -17,11 +21,12 @@ public class ExtractPropertiesProcessor {
     }
 
     public void process(final Exchange ex) throws JsonProcessingException {
-        log.info("property json: {}", property);
+        log.debug("property json: {}", property);
         var jsonNode = MAPPER.readTree(property);
+        log.debug("JsonNode: {}", jsonNode);
         var iterator = jsonNode.fields();
         iterator.forEachRemaining(node -> {
-            log.info("setting property: {} -> {}", node.getKey(), node.getValue().asText(""));
+            log.debug("setting property: {} -> {}", node.getKey(), node.getValue().asText(""));
             ex.getProperties().put(node.getKey(), node.getValue().asText(""));
         });
         log.info("exchange props: {}", ex.getProperties() );
---
+++ src/test/java/com/modusbox/kamelets/ExtractPropertiesProcessorTest.java
@@ -0,0 +1,46 @@
+package com.modusbox.kamelets;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.support.DefaultExchange;
+import org.apache.camel.support.DefaultMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class ExtractPropertiesProcessorTest {
+    @InjectMocks
+    ExtractPropertiesProcessor processor;
+
+    @Test
+    void testProcess() throws JsonProcessingException {
+        var exchange = buildDefaultExchange(null);
+        var property = "{'k1': 'v1', 'k2': 'v2'}";
+        processor.setProperty(property);
+        processor.process(exchange);
+    }
+
+    private static Exchange buildDefaultExchange(Object body) {
+        var timestamp = System.currentTimeMillis();
+        var context = new DefaultCamelContext();
+        var exchange = new DefaultExchange(context);
+        var message = new DefaultMessage(context);
+        message.setMessageId(UUID.randomUUID().toString());
+        message.setBody(body);
+        message.setHeader("header1", "hdr-value-1");
+        message.setHeader(Exchange.MESSAGE_TIMESTAMP, timestamp);
+
+        exchange.setException(null);
+        exchange.setExchangeId(UUID.randomUUID().toString());
+        exchange.setMessage(message);
+        exchange.setProperties(Collections.singletonMap("prop1", "prop-value-1"));
+
+        return exchange;
+    }
+}
